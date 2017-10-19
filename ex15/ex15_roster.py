3271fe0983a0869f60b468ab89b12ef8d5cd135f
thmstm <30295674+thmstm@users.noreply.github.com>
2017-10-20 00:15:31 +0200

    ex15 - roster (updates)

--- ex15/ex15_roster.py
+++ ex15/ex15_roster.py
@@ -1,10 +1,18 @@
+#===============================================================================
+# This application will read roster data in JSON format, parse the file, and
+# then produce an SQLite database that contains a User, Course, and Member
+# table and populate the tables from the data file.
+#===============================================================================
+
+# Importing libraries
 import json
 import sqlite3
 
+#--- Creating and connecting to database
 conn = sqlite3.connect('rosterdb.sqlite')
 cur = conn.cursor()
 
-# Do some setup
+#--- Initialising database
 cur.executescript('''
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Member;
@@ -28,24 +36,32 @@ CREATE TABLE Member (
 )
 ''')
 
+#--- Asking user to input the JSON data file name and provide 'roster_data_sample.json' as a fallback
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data_sample.json'
+    fname = 'roster_data.json'
 
+#--- The structure of the JSON object is the following:
 # [
 #   [ "Charley", "si110", 1 ],
 #   [ "Mea", "si110", 0 ],
 
+#--- Opening and reading the file and putting it into a JSON object
 str_data = open(fname).read()
 json_data = json.loads(str_data)
 
+#--- Looping through the JSON object
 for entry in json_data:
 
+    #--- Looking up the different data fields
     name = entry[0];
     title = entry[1];
+    role = entry[2];
 
-    print((name, title))
+    #--- Printing the data field of the search result JSON object for the user
+    print((name, title, role))
 
+    #--- Updating the relevant tables with the data field of the search result JSON object
     cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
@@ -57,7 +73,8 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''',
-        ( user_id, course_id ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        ( user_id, course_id, role ) )
 
+    #--- Committing the changes
     conn.commit()
